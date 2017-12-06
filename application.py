f639c3f39ba8e2c4f9ef62ef772fe8c03304dcf4
Julie Militza Ortiz <jortiz@college.harvard.edu>
2017-12-06 21:32:23 +0000

    git commit -m "

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import apology, login_required, lookup, usd
+from helpers import apology, login_required
 
 # Configure application
 app = Flask(__name__, template_folder='templates')
@@ -32,10 +32,62 @@ db = SQL("sqlite:///walkly.db")
 @app.route("/history")
 @login_required
 def history():
-    """Show history of transactions"""
+    return render_template("history.html")
 
-    return apology("TODO")
+@app.route("/chance", methods=["GET", "POST"])
+@login_required
+def chance():
+    return render_template("chance.html")
 
+@app.route("/lucy", methods=["GET", "POST"])
+@login_required
+def lucy():
+    return render_template("lucy.html")
+
+@app.route("/fluffy", methods=["GET", "POST"])
+@login_required
+def fluffy():
+    return render_template("fluffy.html")
+
+@app.route("/ineedhelp", methods=["GET", "POST"])
+@login_required
+def ineedhelp():
+    return render_template("ineedhelp.html")
+
+@app.route("/addpet", methods=["GET", "POST"])
+@login_required
+def addpet():
+    if request.method == "POST":
+        pet_data = db.execute("SELECT * FROM Pets WHERE Pet_Identifier = :unique",
+                          unique=request.form.get("Pet_Identifier"))
+        if len(pet_data) == 1:
+            return apology("pet previously added", 403)
+        db.execute("INSERT INTO Pets (Name, Age, Allergies, Favorite_Snack, Additional_Notes, Animal, Pet_Identifier) VALUES (:name, :age, :allergies, :snack, :notes, :Animal, :unique)", name=request.form.get("Name"), age=request.form.get("Age"), allergies=request.form.get("Allergies"), snack=request.form.get("Favorite_Snack"), notes=request.form.get("Additional_Notes"), Animal=request.form.get("Animal"), unique=request.form.get("Pet_Identifier"))
+        return redirect("/addpet")
+    else:
+        pet_data = db.execute("SELECT * FROM Pets WHERE Pet_Identifier = :unique",
+                          unique=request.form.get("Pet_Identifier"))
+        return render_template("addpet.html")
+
+@app.route("/deletepet", methods=["GET", "POST"])
+@login_required
+def delpet():
+    if request.method == "POST":
+        db.execute("DELETE FROM Pets WHERE Pet_Identifier = :unique", unique=request.form.get("Pet_Identifier"))
+        return redirect("/deletepet")
+    else:
+        return render_template("deletepet.html")
+
+@app.route("/friendsinneed")
+@login_required
+def friendsinneed():
+    return render_template("friendsinneed.html")
+
+@app.route("/command", methods=["GET", "POST"])
+@login_required
+def command():
+    Pet = db.execute("SELECT * FROM Pets")
+    return render_template("command.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -47,7 +99,7 @@ def login():
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
 
-        # Ensure username was submittedF
+        # Ensure username was submitted
         if not request.form.get("username"):
             return apology("must provide username", 403)
 
@@ -67,12 +119,11 @@ def login():
         session["user_id"] = rows[0]["id"]
 
         # Redirect user to home page
-        return redirect("login.html")
+        return redirect("/command")
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
-
 
 @app.route("/logout")
 def logout():
@@ -81,8 +132,7 @@ def logout():
     session.clear()
 
     # Redirect user to login form
-    return redirect("login.html")
-
+    return redirect("/login")
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -114,7 +164,6 @@ def register():
 def errorhandler(e):
     """Handle error"""
     return apology(e.name, e.code)
-
 
 # listen for errors
 for code in default_exceptions:
--- helpers.py
+++ helpers.py
@@ -34,17 +34,4 @@ def login_required(f):
     return decorated_function
 
 
-def lookup(symbol):
-    if (symbol == "AAAA"):
-        return {"name": "Stock A", "price": 28.00, "symbol": "AAAA"}
-    elif (symbol == "BBBB"):
-        return {"name": "Stock B", "price": 14.00, "symbol": "BBBB"}
-    elif (symbol == "CCCC"):
-        return {"name": "Stock C", "price": 2000.00, "symbol": "CCCC"}
-    else:
-        return None
 
-
-def usd(value):
-    """Formats value as USD."""
-    return f"${value:,.2f}"
