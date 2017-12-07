8c9470d8ddeeb4d31574e7ea990f8fcafd0cefb5
Julie Militza Ortiz <jortiz@college.harvard.edu>
2017-12-07 05:03:00 +0000

    Add

--- application.py
+++ application.py
@@ -21,6 +21,7 @@ if app.config["DEBUG"]:
 
 
 # Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
@@ -34,60 +35,19 @@ db = SQL("sqlite:///walkly.db")
 def history():
     return render_template("history.html")
 
-@app.route("/chance", methods=["GET", "POST"])
-@login_required
-def chance():
-    return render_template("chance.html")
-
-@app.route("/lucy", methods=["GET", "POST"])
-@login_required
-def lucy():
-    return render_template("lucy.html")
-
-@app.route("/fluffy", methods=["GET", "POST"])
-@login_required
-def fluffy():
-    return render_template("fluffy.html")
-
 @app.route("/ineedhelp", methods=["GET", "POST"])
 @login_required
 def ineedhelp():
     return render_template("ineedhelp.html")
 
-@app.route("/addpet", methods=["GET", "POST"])
-@login_required
-def addpet():
-    if request.method == "POST":
-        pet_data = db.execute("SELECT * FROM Pets WHERE Pet_Identifier = :unique",
-                          unique=request.form.get("Pet_Identifier"))
-        if len(pet_data) == 1:
-            return apology("pet previously added", 403)
-        db.execute("INSERT INTO Pets (Name, Age, Allergies, Favorite_Snack, Additional_Notes, Animal, Pet_Identifier) VALUES (:name, :age, :allergies, :snack, :notes, :Animal, :unique)", name=request.form.get("Name"), age=request.form.get("Age"), allergies=request.form.get("Allergies"), snack=request.form.get("Favorite_Snack"), notes=request.form.get("Additional_Notes"), Animal=request.form.get("Animal"), unique=request.form.get("Pet_Identifier"))
-        return redirect("/addpet")
-    else:
-        pet_data = db.execute("SELECT * FROM Pets WHERE Pet_Identifier = :unique",
-                          unique=request.form.get("Pet_Identifier"))
-        return render_template("addpet.html")
-
-@app.route("/deletepet", methods=["GET", "POST"])
-@login_required
-def delpet():
-    if request.method == "POST":
-        db.execute("DELETE FROM Pets WHERE Pet_Identifier = :unique", unique=request.form.get("Pet_Identifier"))
-        return redirect("/deletepet")
-    else:
-        return render_template("deletepet.html")
-
-@app.route("/friendsinneed")
-@login_required
-def friendsinneed():
-    return render_template("friendsinneed.html")
-
-@app.route("/command", methods=["GET", "POST"])
+@app.route("/command")
 @login_required
 def command():
-    Pet = db.execute("SELECT * FROM Pets")
-    return render_template("command.html")
+    personals = db.execute("SELECT Name, Age, Allergies, Favorite_Snack FROM Pets")
+    rows = db.execute("SELECT Pets.Name, users.username as UsersName, Pets.Directions, Pets.Points FROM Pets JOIN users WHERE Pets.id = users.id")
+    return render_template("command.html", personals=personals, rows=rows)
+
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -158,7 +118,7 @@ def register():
             session["user_id"] = rows[0]["id"]
             if not rows:
                 return apology("Username already exists!", 400)
-            return redirect("/login")
+            return redirect("/command")
 
 
 def errorhandler(e):
@@ -168,3 +128,32 @@ def errorhandler(e):
 # listen for errors
 for code in default_exceptions:
     app.errorhandler(code)(errorhandler)
+
+@app.route("/addpet", methods=["GET", "POST"])
+@login_required
+def addpet():
+    if request.method == "POST":
+        pet_data = db.execute("SELECT * FROM Pets WHERE id = :id",
+                          id=session["user_id"])
+        db.execute("INSERT INTO Pets (id, Name, Age, Allergies, Favorite_Snack, Additional_Notes, Animal) VALUES (:id, :name, :age, :allergies, :snack, :notes, :Animal)",
+                    id=session["user_id"],
+                    name=request.form.get("Name"),
+                    age=request.form.get("Age"),
+                    allergies=request.form.get("Allergies"),
+                    snack=request.form.get("Favorite_Snack"),
+                    notes=request.form.get("Additional_Notes"),
+                    Animal=request.form.get("Animal"))
+        return redirect("/addpet")
+    else:
+
+        return render_template("addpet.html")
+
+@app.route("/deletepet", methods=["GET", "POST"])
+@login_required
+def delpet():
+    if request.method == "POST":
+        db.execute("DELETE FROM Pets WHERE Name = :name", name=request.form.get("Name"))
+        return redirect("/deletepet")
+    else:
+        return render_template("deletepet.html")
+
